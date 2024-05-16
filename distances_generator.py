d7ab74fd132882337a4dc5d2003f882e1fd31f48
Guilherme Rosada <guilhermer4@al.insper.edu.br>
2024-05-15 23:08:47 -0300

    edit: function recives size to generate matrix of distances

--- distances_generator.py
+++ distances_generator.py
@@ -1,13 +1,13 @@
 import random
 
-N_CITIES = 10
+N_CITIES = 14
 N_DEPOSITS = 4
 
-def distances_to_single_deposit():
-    distances = [[0 for i in range(N_CITIES)] for j in range(N_CITIES)]
+def distances_to_single_deposit(n_clients):
+    distances = [[0 for i in range(n_clients)] for j in range(n_clients)]
 
-    for i in range(N_CITIES):
-        for j in range(i, N_CITIES):
+    for i in range(n_clients):
+        for j in range(i, n_clients):
             d = random.randint(100, 1000)
             if j == i:
                 continue
@@ -15,24 +15,30 @@ def distances_to_single_deposit():
                 distances[i][j] = d
                 distances[j][i] = d
 
-    for row in distances:
-        print(row)
-
     return distances
 
-def distances_to_multiple_deposits():
+def distances_to_multiple_deposits(n_clients, n_deposits):
     distances = [
         [
             random.randint(50, 500) 
-            for i in range(N_DEPOSITS)
-        ] for j in range(N_CITIES)
+            for i in range(n_deposits)
+        ] for j in range(n_clients)
     ]
-
-    print(distances)
 
     return distances
 
 if __name__ == "__main__":
 
-    distances_to_multiple_deposits()
-    distances_to_single_deposit()
+    distances = distances_to_single_deposit(N_CITIES)
+    
+    print('Distances to one deposit (routing):')
+    for row in distances:
+        print(row)
+    
+    distances = distances_to_multiple_deposits(N_CITIES, N_DEPOSITS)
+
+    print('Distances to multiple deposits (attribution):')
+    
+    for row in distances:
+        print(row)
+
